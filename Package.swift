ce66f2648a77601982205c1aa7948680fc4c1930
Joe Bay <joebayld@gmail.com>
2019-10-20 17:19:42 -0700

    Removed unneeded things

--- Package.swift
+++ Package.swift
@@ -6,12 +6,10 @@ let package = Package(
     products: [
         .library(name: "S3", targets: ["S3"]),
         .library(name: "S3Signer", targets: ["S3Signer"]),
-        .library(name: "S3TestTools", targets: ["S3TestTools"])
     ],
     dependencies: [
         .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0"),
         .package(url: "https://github.com/LiveUI/XMLCoding.git", from: "0.1.0"),
-        .package(url: "https://github.com/LiveUI/VaporTestTools.git", from: "0.1.5")
     ],
     targets: [
         .target(name: "S3", dependencies: [
@@ -20,28 +18,8 @@ let package = Package(
             "XMLCoding"
             ]
         ),
-        .target(name: "S3DemoRun", dependencies: [
-            "S3DemoApp"
-            ]
-        ),
-        .target(name: "S3DemoApp", dependencies: [
-            "Vapor",
-            "S3",
-            "S3Signer"
-            ]
-        ),
         .target(name: "S3Signer", dependencies: [
             "Vapor"
-            ]
-        ),
-        .target(name: "S3TestTools", dependencies: [
-            "Vapor",
-            "VaporTestTools",
-            "S3"
-            ]
-        ),
-        .testTarget(name: "S3Tests", dependencies: [
-            "S3"
             ]
         )
     ]
